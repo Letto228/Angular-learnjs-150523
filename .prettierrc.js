681a4d433323682bfe244b61e534f3b9d587d513
Darya Taraskova <TaraskovaDarya@gmail.com>
2023-05-21 15:35:05 +0300

    feat: add new prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,10 +2,11 @@
  * @type {import('prettier').Config}
  */
 module.exports = {
-	trailingComma: 'all',
-	useTabs: true,
-	tabWidth: 4,
-	semi: true,
-	singleQuote: true,
-	printWidth: 120,
+    trailingComma: 'all',
+    useTabs: true,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 120,
+    bracketSpacing: true
 };
