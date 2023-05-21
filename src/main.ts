72ca6c00310c5ec65184e0c28bafc63ab3d8a971
iashchenko.dv <Iashchenko.DV@nsd.ru>
2023-05-22 01:03:04 +0300

    feat: update eslint config

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,9 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
 	.bootstrapModule(AppModule)
 	.catch((err) => console.error(err));
+
+function testOneVar(): void {
+		let a;
+		let b; 
+		let c;
+	}
