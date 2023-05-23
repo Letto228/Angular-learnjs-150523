89804d99ebe57a683dc3f9c1163d67e8c0c81136
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-05-23 13:20:02 +0300

    feat: resolve tecture-2 task

---
+++ src/app/pages/products-list/card/card/card.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {IProduct} from '../../../../shared/products/product.interface';
+import {productMock} from '../../../../shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    product: IProduct = productMock;
+}
