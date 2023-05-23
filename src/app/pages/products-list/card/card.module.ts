4953a8a52132cf5959c939cc52736213171d0199
LaNadia <setsias@gmail.com>
2023-05-23 21:59:13 +0400

    feat: add card component

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -1,8 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {CardComponent} from './card/card.component';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
+    declarations: [CardComponent],
+    imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatListModule],
+    exports: [CardComponent],
 })
 export class CardModule {}
