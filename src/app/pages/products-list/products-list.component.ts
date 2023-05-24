3042a77682533b96c33fbe5bd6f8c9860b828c2a
Resaw <resavv@mail.ru>
2023-05-25 00:58:29 +0300

    feat: added product card component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
+import {productMock} from '../../shared/products/product.mock';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    product = productMock;
+    count = 0;
+
+    onClick(e: Event) {
+        e.stopPropagation();
+        this.count += 1;
+    }
+}
