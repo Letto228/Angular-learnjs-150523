aad1fcb987b02b4afb2152cea63368acc47f8522
antizero58 <antizero91@gmial.com>
2023-05-25 13:52:35 +0300

    feat: lecture-2 add card component

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
+import {productMock} from '../../../shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    readonly product: IProduct = productMock;
+
+    onClick(event: MouseEvent) {
+        event.stopPropagation();
+    }
+}
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -5,4 +5,6 @@ import {Component} from '@angular/core';
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    onClick() {}
+}
