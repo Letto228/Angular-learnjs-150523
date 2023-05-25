b07a41257eba2bd0d327aed98b61a19740a367f8
Malnev Dmitiry <Malnev.D@citilink.ru>
2023-05-25 09:54:19 +0300

    #lecture-2 / Fix after review

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -7,17 +7,7 @@ import {productMock} from '../../../shared/products/product.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly feedbackText: string = 'Кол-во отзывов';
-    readonly ratingText: string = 'Рейтинг товара';
-    readonly priceText: string = 'Цена';
-
-    readonly cardId = productMock._id;
-    readonly cardFeedbackCount = productMock.feedbacksCount.toString();
-    readonly cardSubCategory = productMock.subCategory;
-    readonly cardName = productMock.name;
-    readonly cardPrice = productMock.price.toString();
-    readonly cardRating = productMock.rating.toString();
-    readonly cardImage = productMock.images[0];
+    readonly cardData = productMock;
 
     onClick(): void {
         console.warn(`ADDED TO BASKET`);
