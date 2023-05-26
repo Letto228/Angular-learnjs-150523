0ca0abef74a233093afba93e4ff537e14d6febe8
dpimenova <dpimenova@sigma-it.ru>
2023-05-26 11:35:35 +0300

    feat: change the purchase card logic

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,23 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | undefined;
+    @Output() buy = new EventEmitter<IProduct>();
 
-    onProductBuy(event: Event) {
-        event.stopPropagation();
+    onProductBuy() {
+        if (this.product) {
+            this.buy.emit(this.product);
+        }
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        if (this.product) {
+            return this.product.rating >= starIndex;
+        }
+
+        return false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
+import {IProduct} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly product = productsMock[0];
+
+    getBuyingProduct(product: IProduct) {
+        // eslint-disable-next-line no-console
+        console.log(product);
+    }
+}
