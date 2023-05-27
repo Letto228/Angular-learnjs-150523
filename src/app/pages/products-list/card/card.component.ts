9e53f6a64ce8d2d3dd4365721675766674e31b1d
LaNadia <setsias@gmail.com>
2023-05-27 14:52:02 +0400

    feat: add card & product-list interaction

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+// import {productsMock} from '../../../shared/products/products.mock';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +8,19 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    // eslint-disable-next-line @angular-eslint/no-input-rename
+    @Input('product') product: IProduct | undefined;
+    @Output() isAddedToCart = new EventEmitter<string>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
+        this.isAddedToCart.emit(this.product?._id);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly firstProduct: IProduct = productsMock[0];
+
+    isBuyed(id: string): void {
+        // eslint-disable-next-line no-console
+        console.log('Product is added to cart in Parent by its id:', id);
+    }
+}
