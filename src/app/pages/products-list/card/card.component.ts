9c1cc4a50a4d2f14ad39618a2a6fdf2fc3c73630
Mikhail Belov <bezprobeloff@gmail.com>
2023-05-28 23:14:42 +0300

    feat: add @Input component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, Input} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,7 +7,9 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    // readonly product = productsMock[0];
+    @Input()
+    product: IProduct | undefined;
 
     onProductBuy(event: Event) {
         event.stopPropagation();
@@ -17,6 +19,10 @@ export class CardComponent {
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        if (this.product) {
+            return this.product.rating >= starIndex;
+        }
+
+        return false;
     }
 }
