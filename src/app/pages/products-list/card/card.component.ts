2ed6caaeb7d6ffd32cf90cd94b4e80d60a78752b
Valentina Romats <valentina.romats@veloceapps.com>
2023-05-28 10:23:53 +0300

    feat: modify card component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,18 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | undefined;
+    @Output() productSelected$ = new EventEmitter<IProduct['_id']>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
+        this.productSelected$.emit(this.product!._id);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product!.rating >= starIndex;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,21 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
+import {IProduct} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly products = productsMock;
+
+    trackById(_: number, product: IProduct) {
+        return product._id;
+    }
+
+    addToBasket(productId: IProduct['_id']): void {
+        // eslint-disable-next-line no-console
+        console.log('Add to basket product with Id ', productId);
+    }
+}
