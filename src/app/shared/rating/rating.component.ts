a27a260e9f0547178a8bfeac1dec00f19ef985ff
Valentina Romats <valentina.romats@veloceapps.com>
2023-05-29 17:08:15 +0300

    feat: add review fixes

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,11 +1,17 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-rating',
     templateUrl: './rating.component.html',
     styleUrls: ['./rating.component.css'],
 })
-export class RatingComponent {
+export class RatingComponent implements OnInit {
     @Input() rating = 0;
     @Input() ratingMax = 5;
+
+    range: any[] | undefined;
+
+    ngOnInit() {
+        this.range = this.ratingMax ? Array.from(new Array(this.ratingMax)) : undefined;
+    }
 }
