5cf3d622ce006c6e02b64861c71c933c7e568286
Piatrouski Herman <herman.piatrouski@generali.com>
2023-05-30 21:53:48 +0200

    feat: lecture 3 create input and output for card component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,14 @@
 import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    selectNewProduct(id: IProduct['_id']) {
+        // eslint-disable-next-line no-console
+        console.log(id);
+    }
+}
