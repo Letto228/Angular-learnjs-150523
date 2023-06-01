471559afe9960fe64e19b93af7c2062a8eb3f394
Olha <olhakonst@gmail.com>
2023-06-01 13:57:02 +0200

    feat: create input and output for card

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly products = productsMock;
+
+    getBuyProduct(id: string) {
+        // eslint-disable-next-line no-console
+        console.log(`Purchase product ${id}`);
+    }
+}
