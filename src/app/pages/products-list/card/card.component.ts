f7a416b7ce3c71c133980867794d81da25a5a950
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-06-01 18:21:12 +0300

    chore: sanitize project

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,18 +1,4 @@
-import {
-    Component,
-    EventEmitter,
-    Input,
-    OnChanges,
-    Output,
-    SimpleChanges,
-    OnInit,
-    DoCheck,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked,
-    OnDestroy,
-} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
@@ -20,76 +6,10 @@ import {IProduct} from '../../../shared/products/product.interface';
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent
-    implements
-        OnChanges,
-        OnInit,
-        DoCheck,
-        AfterContentInit,
-        AfterContentChecked,
-        AfterViewInit,
-        AfterViewChecked,
-        OnDestroy
-{
+export class CardComponent {
     @Input() product: IProduct | undefined;
-    @Input() user: string | undefined;
 
     @Output() buy = new EventEmitter<IProduct['_id']>();
-
-    constructor() {
-        // eslint-disable-next-line no-console
-        console.log('constructor CardComponent');
-    }
-
-    ngOnChanges({product, user}: SimpleChanges): void {
-        // eslint-disable-next-line no-console
-        console.log('ngOnChanges CardComponent', product, user);
-
-        // // if (product && !(product.previousValue as IProduct) && product.currentValue) {
-        // // product.currentValue === this.product
-        // if (product && !(product.previousValue as IProduct) && this.product) {
-        //     // focus card
-        // }
-
-        // if (user) {
-        //     // focus user
-        // }
-    }
-
-    ngOnInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('OnInit CardComponent');
-    }
-
-    ngDoCheck(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngDoCheck CardComponent');
-    }
-
-    ngAfterContentInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterContentInit CardComponent');
-    }
-
-    ngAfterContentChecked(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterContentChecked CardComponent');
-    }
-
-    ngAfterViewInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterViewInit CardComponent');
-    }
-
-    ngAfterViewChecked(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterViewChecked CardComponent');
-    }
-
-    ngOnDestroy(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngOnDestroy CardComponent');
-    }
 
     onProductBuy(event: Event) {
         event.stopPropagation();
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {productsMock} from '../../shared/products/products.mock';
-import {IProduct} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
@@ -9,64 +8,4 @@ import {IProduct} from '../../shared/products/product.interface';
 })
 export class ProductsListComponent {
     readonly products = productsMock;
-
-    onProductBuy(id: IProduct['_id']) {
-        // eslint-disable-next-line no-console
-        console.log(id);
-    }
-
-    constructor() {
-        // eslint-disable-next-line no-console
-        console.log('constructor ProductsListComponent');
-    }
-
-    ngOnChanges(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngOnChanges ProductsListComponent');
-
-        // // if (product && !(product.previousValue as IProduct) && product.currentValue) {
-        // // product.currentValue === this.product
-        // if (product && !(product.previousValue as IProduct) && this.product) {
-        //     // focus card
-        // }
-
-        // if (user) {
-        //     // focus user
-        // }
-    }
-
-    ngOnInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('OnInit ProductsListComponent');
-    }
-
-    ngDoCheck(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngDoCheck ProductsListComponent');
-    }
-
-    ngAfterContentInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterContentInit ProductsListComponent');
-    }
-
-    ngAfterContentChecked(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterContentChecked ProductsListComponent');
-    }
-
-    ngAfterViewInit(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterViewInit ProductsListComponent');
-    }
-
-    ngAfterViewChecked(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngAfterViewChecked ProductsListComponent');
-    }
-
-    ngOnDestroy(): void {
-        // eslint-disable-next-line no-console
-        console.log('ngOnDestroy ProductsListComponent');
-    }
 }
