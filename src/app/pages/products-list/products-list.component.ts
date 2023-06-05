8d9437b257ffd34c5634482690b8a4d42b0cf2a0
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-06-05 19:45:56 +0300

    chore: sanitize project

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {productsMock} from '../../shared/products/products.mock';
 import {IProduct} from '../../shared/products/product.interface';
-import {LoadDirection} from '../../shared/scroll-with-loading/load-direction.const';
 
 @Component({
     selector: 'app-products-list',
@@ -21,15 +20,5 @@ export class ProductsListComponent implements OnInit {
         // console.log('calculated products');
 
         return this.products; // sorting
-    }
-
-    onToggle(event: unknown) {
-        // eslint-disable-next-line no-console
-        console.log(event);
-    }
-
-    onLoad(event: LoadDirection) {
-        // eslint-disable-next-line no-console
-        console.log(`load ${event}`);
     }
 }
