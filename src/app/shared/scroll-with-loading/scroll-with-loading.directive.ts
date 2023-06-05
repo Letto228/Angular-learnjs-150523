2dda6d79f8e7d50cb5004687c5b27cef9de342f8
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-06-05 19:40:51 +0300

    feat: lecture 5 additional solution with uniq event

--- src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -3,6 +3,8 @@ import {LoadDirection} from './load-direction.const';
 import {isScrollReachedBottomOffcet} from './utils/is-scroll-reached-bottom-offcet';
 import {isScrollReachedTopOffcet} from './utils/is-scroll-reached-top-offcet';
 
+// Without uniq event
+// ------------------
 @Directive({
     selector: '[appScrollWithLoading]',
 })
@@ -37,3 +39,60 @@ export class ScrollWithLoadingDirective {
         }
     }
 }
+
+// With uniq event
+// ---------------
+// @Directive({
+//     selector: '[appScrollWithLoading]',
+// })
+// export class ScrollWithLoadingDirective {
+//     @Output() loadData = new EventEmitter<LoadDirection>();
+
+//     private prevScrollTop = -1;
+//     private locationArea = {
+//         [LoadDirection.Before]: true,
+//         [LoadDirection.After]: false,
+//     };
+
+//     @HostListener('scroll', ['$event.target'])
+//     onScroll({scrollTop, clientHeight, scrollHeight}: HTMLElement) {
+//         const previousLocationArea = this.locationArea;
+
+//         this.updateLocationArea(scrollTop, clientHeight, scrollHeight);
+
+//         const insertInTopOffset =
+//             this.locationArea[LoadDirection.Before] && !previousLocationArea[LoadDirection.Before];
+
+//         if (insertInTopOffset) {
+//             this.loadData.emit(LoadDirection.After);
+
+//             return;
+//         }
+
+//         const insertInBottomOffset =
+//             this.locationArea[LoadDirection.After] && !previousLocationArea[LoadDirection.After];
+
+//         if (insertInBottomOffset) {
+//             this.loadData.emit(LoadDirection.Before);
+//         }
+//     }
+
+//     private updateLocationArea(scrollTop: number, clientHeight: number, scrollHeight: number) {
+//         const prevScrollTop = this.prevScrollTop;
+
+//         this.prevScrollTop = scrollTop;
+
+//         const lowerScrollPosition = scrollHeight - clientHeight;
+//         const shouldLoadMessagesDown = isScrollReachedBottomOffcet(
+//             scrollTop,
+//             lowerScrollPosition,
+//             prevScrollTop,
+//         );
+//         const shouldLoadMessagesTop = isScrollReachedTopOffcet(scrollTop, prevScrollTop);
+
+//         this.locationArea = {
+//             [LoadDirection.After]: shouldLoadMessagesDown,
+//             [LoadDirection.Before]: shouldLoadMessagesTop,
+//         };
+//     }
+// }
