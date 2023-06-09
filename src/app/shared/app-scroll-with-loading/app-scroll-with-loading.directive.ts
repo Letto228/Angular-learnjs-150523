8f2b61384c797a9a0f26604c893f63307c1b11b4
LaNadia <setsias@gmail.com>
2023-06-09 18:58:56 +0400

    feat: add mousewheel listener

--- src/app/shared/app-scroll-with-loading/app-scroll-with-loading.directive.ts
+++ src/app/shared/app-scroll-with-loading/app-scroll-with-loading.directive.ts
@@ -7,9 +7,15 @@ import {LoadDirection} from './app-scroll-with-loading.enum';
 })
 export class AppScrollWithLoadingDirective implements OnInit, OnDestroy {
     @Output() loadData = new EventEmitter<LoadDirection>();
-    // private readonly borderOffsetTop$ = new Subject<number>();
+    private readonly borderOffsetTop$ = new Subject<number>();
     private readonly borderOffsetBottom$ = new Subject<number>();
     private readonly destroy$ = new Subject<void>();
+    private deltaY = 0;
+
+    @HostListener('wheel', ['$event'])
+    onWheel(event: WheelEvent) {
+        this.deltaY = (<WheelEvent>event).deltaY;
+    }
 
     @HostListener('scroll', ['$event.target'])
     onScroll({scrollTop, scrollHeight, offsetHeight}: HTMLElement) {
@@ -19,8 +25,13 @@ export class AppScrollWithLoadingDirective implements OnInit, OnDestroy {
         // вычисляем, сколько пикс до нижней границы
         const borderOffset = invisibleOffset - scrollTop;
 
-        this.borderOffsetBottom$.next(borderOffset);
-        // this.borderOffsetTop$.next(scrollTop);
+        if (this.deltaY < 0) {
+            this.borderOffsetTop$.next(scrollTop);
+        }
+
+        if (this.deltaY > 0) {
+            this.borderOffsetBottom$.next(borderOffset);
+        }
     }
 
     ngOnInit() {
@@ -38,13 +49,13 @@ export class AppScrollWithLoadingDirective implements OnInit, OnDestroy {
             });
 
         // подписка на отступ сверху
-        // this.borderOffsetTop$
-        //     .pipe(debounceTime(100), takeUntil(this.destroy$))
-        //     .subscribe((pixels: number) => {
-        //         if (pixels <= 100) {
-        //             this.loadData.emit(LoadDirection.SCROLLUP);
-        //         }
-        //     });
+        this.borderOffsetTop$
+            .pipe(debounceTime(100), takeUntil(this.destroy$))
+            .subscribe((pixels: number) => {
+                if (pixels <= 100) {
+                    this.loadData.emit(LoadDirection.SCROLLUP);
+                }
+            });
     }
 
     ngOnDestroy() {
