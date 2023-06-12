3065a90b6bbc5dd9ee974f7be40aa3f81c4cf0bc
tshimber <tatiana.shimbereva@telekom.de>
2023-06-12 18:39:07 +0200

    feat: product as input

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly products = productsMock;
+
+    handleBuyBtnClicked(productId: string): void {
+        // eslint-disable-next-line no-console
+        console.log(`Product list: product with id ${productId} was bought`);
+    }
+}
