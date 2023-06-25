a2ce2586ddcb93a9e783300d16c373ed203f5dc9
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-06-25 13:37:51 +0300

    feat: product-list/:subcat routing

--- src/app/components/sidenav/categories-select/categories-select.component.ts
+++ src/app/components/sidenav/categories-select/categories-select.component.ts
@@ -1,5 +1,7 @@
+import {ActivatedRoute, Router} from '@angular/router';
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {ICategory} from '../../../shared/categories/category.interface';
+import {ProductsStoreService} from '../../../shared/products/products-store.service';
 
 @Component({
     selector: 'app-categories-select',
@@ -9,4 +11,28 @@ import {ICategory} from '../../../shared/categories/category.interface';
 })
 export class CategoriesSelectComponent {
     @Input() categories!: ICategory[] | null;
+
+    constructor(
+        private readonly productsStoreService: ProductsStoreService,
+
+        private readonly activatedRoute: ActivatedRoute,
+
+        private readonly router: Router,
+    ) {}
+
+    subCategorySelected(_id: string) {
+        this.productsStoreService.loadProducts(_id);
+
+        this.navigateToTab(_id);
+    }
+
+    navigateToTab(_id: string) {
+        this.router.navigate([`./products-list/${_id}`], {relativeTo: this.activatedRoute});
+
+        // const urlTree = this.router.createUrlTree([`./${tab}`], {relativeTo: this.activatedRoute});
+
+        // console.log(urlTree.toString(), urlTree);
+
+        // this.router.navigateByUrl(urlTree);
+    }
 }
