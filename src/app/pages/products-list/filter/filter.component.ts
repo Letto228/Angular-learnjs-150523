d00cd021d04797f0e6d485a0fb8260cadef72f0b
LaNadia <setsias@gmail.com>
2023-06-29 10:28:05 +0400

    feat: search name by input + add queryparams

--- src/app/pages/products-list/filter/filter.component.ts
+++ src/app/pages/products-list/filter/filter.component.ts
@@ -8,6 +8,7 @@ import {
     SimpleChanges,
 } from '@angular/core';
 import {FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
 import {IProductsFilter} from './products-filter.interface';
 
 @Component({
@@ -18,8 +19,38 @@ import {IProductsFilter} from './products-filter.interface';
 })
 export class FilterComponent implements OnChanges {
     @Input() brands: string[] | null = null;
-
+    // вариант 2
     @Output() changeFilter = new EventEmitter<IProductsFilter>();
+    sendForm() {
+        if (this.filterForm) {
+            this.changeFilter.emit({
+                name: this.filterForm.value.search ? this.filterForm.value.search : '',
+                brands: [],
+                priceRange: {
+                    min: this.filterForm.value.priceRange?.min
+                        ? this.filterForm.value.priceRange?.min
+                        : 1,
+                    max: this.filterForm.value.priceRange?.max
+                        ? this.filterForm.value.priceRange?.max
+                        : 9999,
+                },
+            });
+        }
+    }
+
+    // вариант 1
+    // @HostListener('keyup', ['$event.target.value'])
+    // onClick(value: string | null) {
+    //     value?.length === 0 ? this.setQueryparams(null) : this.setQueryparams(value);
+    // }
+
+    // setQueryparams(param: string | null) {
+    //     this.router.navigate(['products-list'], {
+    //         queryParams: {
+    //             search: param,
+    //         },
+    //     });
+    // }
 
     readonly filterForm = this.formBuilder.group({
         search: '',
@@ -30,7 +61,7 @@ export class FilterComponent implements OnChanges {
         }),
     });
 
-    constructor(private readonly formBuilder: FormBuilder) {}
+    constructor(private readonly formBuilder: FormBuilder, private readonly router: Router) {}
 
     ngOnChanges({brands}: SimpleChanges): void {
         if (brands) {
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,9 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {map, tap, switchMap} from 'rxjs';
 import {IProduct} from '../../shared/products/product.interface';
 import {ProductsStoreService} from '../../shared/products/products-store.service';
 import {BrandsService} from '../../shared/brands/brands.service';
+import {IProductsFilter} from './filter/products-filter.interface';
 
 @Component({
     selector: 'app-products-list',
@@ -28,10 +29,33 @@ export class ProductsListComponent {
         switchMap(() => this.brandsService.brands$),
     );
 
+    //  вариант 1
+    // readonly searchQuery$ = this.activatedRoute.queryParamMap.pipe(
+    //     map(queryparams => (queryparams.get('search') !== null ? queryparams.get('search') : '')),
+    // );
+
+    //  вариант 2
+    searchQuery = '';
+    setSearch(form: IProductsFilter) {
+        this.setQueryparams(form);
+    }
+
+    setQueryparams(form: IProductsFilter) {
+        this.router.navigate(['products-list'], {
+            queryParams: {
+                search: form.name,
+                min: form.priceRange.min,
+                max: form.priceRange.max,
+            },
+        });
+        this.searchQuery = form.name;
+    }
+
     constructor(
         private readonly productsStoreService: ProductsStoreService,
         private readonly activatedRoute: ActivatedRoute,
         private readonly brandsService: BrandsService,
+        private readonly router: Router,
     ) {}
 
     trackBy(_index: number, item: IProduct) {
